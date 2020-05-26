0a3482c5b619b45302e99ace81debdd015bd7f45
Adithya Selvaprithiviraj <adithyaselv@gmail.com>
2020-05-26 17:51:32 -0400

    Updates

---
+++ precache-manifest.a209cd2545c4ab894d4ae8e60ea4fb8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a98c60702f7c254f7df9a5e4d518913b",
+    "url": "/Meiyaluma/index.html"
+  },
+  {
+    "revision": "5bbcc2fcd70584a3a7ee",
+    "url": "/Meiyaluma/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3bf7517d0b51308eee42",
+    "url": "/Meiyaluma/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5bbcc2fcd70584a3a7ee",
+    "url": "/Meiyaluma/static/js/2.e98d0e8f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Meiyaluma/static/js/2.e98d0e8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bf7517d0b51308eee42",
+    "url": "/Meiyaluma/static/js/main.f1a3c97b.chunk.js"
+  },
+  {
+    "revision": "385b931e9ca329efd811",
+    "url": "/Meiyaluma/static/js/runtime-main.dce4856f.js"
+  }
+]);
