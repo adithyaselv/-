2a603e965bd79dd93753b22ce43d20d4453631ef
Adithya Selvaprithiviraj <adithyaselv@gmail.com>
2020-05-27 20:20:01 -0400

    Add logic to handle selected files and upload

--- frontend/src/UploadForm.js
+++ frontend/src/UploadForm.js
@@ -1,11 +1,34 @@
 import React, {Component} from 'react';
 import Button from 'react-bootstrap/Button';
 import {Form, Row} from 'react-bootstrap';
+import axios from 'axios';
 
 class UploadForm extends Component {
 
+    state = {
+        selectedFile: null,
+        label: "Choose your meme image"
+    }
+
+    fileOnChange = (event) => {
+        // TODO: Validate number of files , file size, file type
+        this.setState ({
+            selectedFile: event.target.files[0],
+            label: event.target.files[0].name
+        })
+    }
+
     handleFormSubmit = () => {
-        alert("uploaded")
+        const formData = new FormData;
+        formData.append('file', this.state.selectedFile);
+
+        axios.post("http://localhost:3001/upload", formData) 
+        .then(res => { 
+            alert("upload success")
+        })
+        .catch(err => {
+            alert("upload failed")
+        })
     }
 
     render () {
@@ -13,7 +36,7 @@ class UploadForm extends Component {
             <Form >
             <Form.Group >  
             <Row>
-                <Form.File id="imagefile" label="Choose your meme image" custom/>
+                <Form.File id="imagefile" label={this.state.label} custom onChange={this.fileOnChange} />
             </Row>
             </Form.Group>
             <Form.Group>
