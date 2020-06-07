4945b51b6fc3f2dd8243474b142d8d068cfca7f0
Adithya Selvaprithiviraj <adithyaselv@gmail.com>
2020-06-06 21:46:32 -0400

    Add ocr in meme-text-service

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# மெய்யாலும்மா?  (மெய் ஆளுமா?)
+# மெய்யாலுமா?  (மெய் ஆளுமா?)
 
 Fact checker for memes shared in social media
--- meme-text-service/meme-text-service.py
+++ meme-text-service/meme-text-service.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from flask import request
 from flask_cors import CORS
+from pytesseract import image_to_string
+from PIL import Image
 
 app = Flask(__name__)
 CORS(app)
 
 
-def meme_to_test(image_file):
-    return "வாமா மின்னல்"
+def meme_to_test(image_file_path):
+    image_file = Image.open(image_file_path)
+    return image_to_string(image_file, lang = 'tam')
 
 
 @app.route('/')
@@ -16,8 +19,8 @@ def home():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    image_file = request.files['file']
-    meme_text = meme_to_test(image_file)
+    image_file_path = request.files['file']
+    meme_text = meme_to_test(image_file_path)
     return meme_text
 
 # Todo : Use WYSWYG server
